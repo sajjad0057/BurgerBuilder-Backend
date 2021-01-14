6dcd24c1c54246648504cbdfca8e5c99c2716d65
sajjad Hossain <sajjadhossain0057@gamil.com>
2021-01-14 06:19:02 +0600

    Filtering Orders and add IsAuthenticated permission

--- BurgerAPI/views.py
+++ BurgerAPI/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
 from .models import UserProfile, Order,CustomerDetail,Ingredient
 from .serializers import UserProfileSerializer, OrderSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -13,3 +14,24 @@ class UserProfileViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+    
+    permission_classes = [
+        #IsAuthenticated,
+    ]
+    
+    
+    # according To filtering Documentation in DRF :
+    def get_queryset(self):
+        queryset = Order.objects.all()
+        id = self.request.query_params.get("id",None)
+        if id is not None :
+            queryset = queryset.filter(user__id=id)
+            
+        return queryset
+    
+'''
+# By this query_set http://example.com/api/order?id=2  means thats link show just order id 2
+if we don't pass ?id=2 end of this url,when show all orders
+
+'''
+    
